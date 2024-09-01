ebf87ee0a59046146f65b3950e4a36610b0e8211
Peter Kidson <peterdkidson@googlemail.com>
2024-09-01 06:30:13 +0100

    To 31

---
+++ 02-PHP-ESSENTIALS/01-strings/strings.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./simple.css" />
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+echo "Welcome back!\nWe're glad to see you again.<br/>Check out our new features.";	 ?>
+?>
+
+
+    <pre><?php
+
+$greeting = 'I\'m a string';
+echo $greeting . '!!! ' . $greeting;
+echo '<br />';
+
+$name = 'Jannis';
+$subject = 'PHP';
+
+$text = 'I\'m ' . $name . ' and I want to learn the language ' . $subject;
+echo $text . '!';
+
+
+    ?></pre>
+<br>
+</body>
+</html>
---
+++ 02-PHP-ESSENTIALS/01-strings/x.php
@@ -0,0 +1,22 @@
+<?php
+
+
+if (!isset($selectedCoffee) || empty($selectedCoffee)) $selectedCoffee = 'drip';
+
+?>
+
+<div class="coffee-info">
+	<?php if ($selectedCoffee == 'espresso'): ?>
+		<div id="espresso-info">
+			<h1>Espresso ☕</h1>
+			<p>Espresso is a concentrated coffee drink with a bold flavor. It pairs perfectly with a chocolate croissant. 🍫🥐</p>
+		</div>
+	<?php endif; ?>
+	<?php if ($selectedCoffee == 'drip'): ?>
+		<div id="drip-coffee-info">
+			<h1>Drip Coffee ☕</h1>
+			<p>Drip coffee, a staple in many routines, is known for its straightforward brewing process and comforting, familiar taste. Perfect for starting your morning or as a midday pick-me-up. ☕️🌅</p>
+		</div>
+	<?php endif; ?>
+</div>
+
