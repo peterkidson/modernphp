9e5a0a6cbb4cf4e0be4168886d7beacd0a207f3f
Peter Kidson <peterdkidson@googlemail.com>
2024-11-08 07:43:57 +0000

    reorging...

--- 06-URL-PARAMS/49.php
+++ 06-URL-PARAMS/49.php
@@ -3,14 +3,13 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="./simple.css" />
+	<link rel="stylesheet" type="text/css" href="simple.css" />
 	<title>Document</title>
 </head>
 <body>
 	<pre>
 		<?php
 			var_dump($_GET);
-			echo $_GET[0];
 		?>
 	</pre>
 
