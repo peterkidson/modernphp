db06fea6bbd49b62bfb0e74ed5b35363f6eaee65
Peter Kidson <peterdkidson@googlemail.com>
2024-12-20 09:24:31 +0000

    62 Part 2 (Design the Gallery Layout)

---
+++ 07-IMAGE-GALLERY/mine/gallery.php
@@ -0,0 +1,19 @@
+<?php
+include './inc/functions.inc.php';
+include './inc/images.inc.php';
+
+// Part 1 (Display the Images in a Gallery View)
+
+?>
+<?php include './views/header.php'; ?>
+
+<div class="gallery-container">
+	<?php foreach( $imageTitles as $img => $title) : ?>
+		<a href="image.php?<?php echo http_build_query(['image' => $img]); ?>" class="gallery-item">
+			<h3><?php echo e($title); ?></h3>
+			<img src="./images/<?php echo rawurlencode($img); ?>" alt="<?php echo e($title); ?>" />
+		</a>
+	<?php endforeach; ?>
+</div>
+
+<?php include './views/footer.php'; ?>
