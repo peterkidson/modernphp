b280a0e5c722f90c6133d4650d434fd879ac375e
Peter Kidson <peterdkidson@googlemail.com>
2024-12-26 09:35:40 +0000

    70 includes

---
+++ 08-ADVANCED/70-includes/includes.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="./simple.css" />
+	<title>Document</title>
+</head>
+<body>
+
+<pre><?php
+
+	require_once __DIR__ . '/inc/functions.inc.php';
+	require_once __DIR__ . '/inc/functions.inc.php';
+
+	$text = "hello world";
+
+	?></pre>
+
+<h3><?php echo e($text); ?></h3>
+
+</body>
+</html>
