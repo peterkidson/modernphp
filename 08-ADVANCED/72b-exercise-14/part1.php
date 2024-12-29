86df65a389fa3f62e67b6ab8d86bf6fd859ceeb7
Peter Kidson <peterdkidson@googlemail.com>
2024-12-29 11:18:15 +0000

    72b exercise 14 part1

---
+++ 08-ADVANCED/72b-exercise-14/part1.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="./simple.css" />
+	<title>Document</title>
+</head>
+<body>
+
+<pre><?php
+
+	require_once __DIR__ . '/inc/functions.inc.php';
+
+	$emailContent = "Subject: Unlock Your Potential with Us!\n\nDear Alex,\n\nWe hope this message finds you well.\n\nQuote of the Month:\n\nDr. Albert Szent-Györgyi: 'Innovation is seeing what everybody has seen and thinking what nobody has thought.'\n\nBest wishes,\nYour Discovery Network Team\nP.S. Don't miss our special announcement next month!";
+
+
+	$splits = explode("\n", $emailContent);
+
+	$qotm 		= "Quote of the Month:";
+	$modifiedSplits = $splits;
+
+	foreach ($splits as $key => $value) {
+		if ($value == $qotm) {
+			$authorPlusQuote = $splits[$key+2];
+			$apq = explode(':', $authorPlusQuote);
+			$author  = trim($apq[0]);
+			$quote   = trim($apq[1]);
+			$modifiedSplits[$key+2] = "$quote - $author";
+			break;
+		}
+	}
+
+	$modifiedEmailContent = implode("\n", $modifiedSplits);
+	var_dump($modifiedSplits);
+
+
+
+?></pre>
+
+</body>
+</html>
