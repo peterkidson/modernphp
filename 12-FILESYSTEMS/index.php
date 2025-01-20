854b8a0cc4b88c674977e0eb12bb0222cb4ad09b
Peter Kidson <peterdkidson@googlemail.com>
2025-01-20 06:02:20 +0000

    96 read a dir, while loop

--- 12-FILESYSTEMS/index.php
+++ 12-FILESYSTEMS/index.php
@@ -1,31 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="./styles/simple.css" />
-    <title>Document</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="./styles/simple.css" />
+	<title>Document</title>
 </head>
 <body>
-    <header><h1>Automatic Image List</h1></header>
-    <main><pre><?php 
-			$handle = opendir(__DIR__ . '/images');
-			var_dump($handle);
-			$X = readdir($handle);
-			$X = readdir($handle);
-			$X = readdir($handle);
-			$X = readdir($handle);
-			$X = readdir($handle);
-			$X = readdir($handle);
-			$X = readdir($handle);
-			var_dump(readdir($handle));
-			var_dump(readdir($handle));
-			var_dump(readdir($handle));
-			var_dump(readdir($handle));
-			var_dump(readdir($handle));
-			var_dump(readdir($handle));
-			var_dump(readdir($handle));
-			closedir($handle);
-    ?></pre></main>
+<header><h1>Automatic Image List</h1></header>
+<main><pre><?php
+		$handle = opendir(__DIR__ . '/images');
+		/*
+		$currentFile = readdir($handle);
+		while ($currentFile !== false) {
+			 var_dump($currentFile);
+			 $currentFile = readdir($handle);
+		}
+		*/
+		$images = [];
+		while (($currentFile = readdir($handle)) !== false) {
+			if ($currentFile === '.' || $currentFile === '..') {
+				continue;
+			}
+			var_dump($currentFile);
+			$images[] = $currentFile;
+		}
+
+		// var_dump($str2 = $str = 'hello');
+
+	foreach($images AS $image) {
+		$src = "images/" . rawurlencode($image);
+		echo "";
+	}
+
+
+		closedir($handle);
+		?></pre>
+
+	<?php foreach($images AS $image): ?>
+		<img src="images/<?= rawurlencode($image); ?>" alt="ohoh" />
+	<?php endforeach; ?>
+
+</main>
 </body>
 </html>
