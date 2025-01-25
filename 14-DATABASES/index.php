ce67913a83b7d29f4ead867f8a663ab2b63c8c23
Peter Kidson <peterdkidson@googlemail.com>
2025-01-25 08:28:35 +0000

    120 update & delete

--- 14-DATABASES/index.php
+++ 14-DATABASES/index.php
@@ -2,33 +2,54 @@
 
 require_once __DIR__ . '../../inc/functions.inc.php';
 
+function plist($pdo)
+{
+	$query = $pdo->prepare('SELECT * FROM notes ');
+	$query->execute();
+	$results = $query->fetchAll(PDO::FETCH_ASSOC);
+	?>
+
+	<ul>
+		<?php foreach( $results as $note): ?>
+			<li><?= e($note['title']) ?></li>
+		<?php endforeach; ?>
+	</ul>
+
+	<?php
+}
+
 try {
 	$pdo = new PDO('mysql:host=localhost;dbname=note_app', 'root', '',
 			[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 }
 catch (PDOException $e) {
 	echo $e->getMessage();
+	die();
 }
-$title = 'x title';
-$content = 'x content';
 
+plist($pdo);
 
-$query = $pdo->prepare('INSERT INTO `notes` (title, content) VALUES (:ptitle, :pcontent)');
+$id		= 3;
+$title 	= 'u title';
+$content	= 'u content';
+
+$query = $pdo->prepare('UPDATE notes SET title = :ptitle, content = :pcontent WHERE id > :pid');
 $query->bindValue(':ptitle', $title);
 $query->bindValue(':pcontent', $content);
+$query->bindValue(':pid', $id);
 $query->execute();
 
+plist($pdo);
 
-$query = $pdo->prepare('SELECT * FROM notes ');
+$query = $pdo->prepare('INSERT INTO `notes` (title, content) VALUES ("hello", "world")');
 $query->execute();
-$results = $query->fetchAll(PDO::FETCH_ASSOC);
+
+plist($pdo);
+
+$query = $pdo->prepare('DELETE FROM notes WHERE title = :ptitle');
+$query->bindValue(':ptitle', $title);
+$query->execute();
+
+plist($pdo);
 
 echo "";
-
-?>
-
-<ul>
-	<?php foreach( $results as $note): ?>
-		<li><?= e($note['title']) ?></li>
-	<?php endforeach; ?>
-</ul>
