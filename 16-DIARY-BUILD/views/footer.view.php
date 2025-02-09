37584ea3438e88c6692d931de9f7309ac6744ebc
Peter Kidson <peterdkidson@googlemail.com>
2025-02-09 08:12:01 +0000

    146 form page

---
+++ 16-DIARY-BUILD/views/footer.view.php
@@ -0,0 +1,16 @@
+</div>
+
+</main>
+
+<footer class="footer">
+	<div class="container">
+		<h3 class="footer__heading">PHP Diary</h3>
+		<p class="footer__desc">
+			INDEX.php How to use
+			SELMS Consortium
+		</p>
+	</div>
+</footer>
+
+</body>
+</html>
