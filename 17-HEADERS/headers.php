dd48d19894eabd1bcbb07159c2e1484c527bfed4
Peter Kidson <peterdkidson@googlemail.com>
2025-02-27 06:50:36 +0000

    162 headers : server to browser

---
+++ 17-HEADERS/headers.php
@@ -0,0 +1,18 @@
+<?php
+
+header('Content-Type: text/plain');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Title</title>
+</head>
+<body>
+<h1>The Title</h1>
+</body>
+</html>
+
+
---
+++ 17-HEADERS/image.php
@@ -0,0 +1,8 @@
+<?php
+
+header('Content-Type: image/jpeg');
+
+readfile(__DIR__ . '../../16-DIARY-BUILD/images/IMG_0294.jpeg');
+
+?>
+
