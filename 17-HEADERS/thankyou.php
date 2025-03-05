0729af0b0eaa790ad440539477368439d49fbba1
Peter Kidson <peterdkidson@googlemail.com>
2025-03-05 07:20:41 +0000

    167 headers : signup and redirect

---
+++ 17-HEADERS/thankyou.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href=__DIR__ . "../../inc/simple.css" />
+	<title>Document 167</title>
+</head>
+<main>
+	<header><h1>Sign up for the newsletter</h1></header>
+	<main?<h1>Thank you for signing</h1></main>
+</body>
+</html>
