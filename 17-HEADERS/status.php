6f1b202b3fd989525bcfdf1aebdc7f2362f81dc6
Peter Kidson <peterkidson-git@spamarrest.com>
2025-06-12 05:42:10 +0100

    169 Custom 404  part 1

---
+++ 17-HEADERS/status.php
@@ -0,0 +1,24 @@
+<?php
+require __DIR__ . '../../inc/functions.inc.php';
+
+if ( ($id = $_GET['id'] ?? -1) > 5) {
+    require './status/notfound.php';
+    die();
+};
+
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="simplt.css" />
+  <title>Title</title>
+</head>
+<body>
+<header>
+    <main>
+        The id is <?= e($id) ?>
+    </main>
+</header>
+</body>
+</html>
