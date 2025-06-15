902696ecf827092055f20c067658e1500edf775a
Peter Kidson <peterkidson-git@spamarrest.com>
2025-06-15 10:37:12 +0100

    187 Import the data

---
+++ 20-NAME-EXPLORER/db-connect.php
@@ -0,0 +1,10 @@
+<?php
+
+try {
+   $pdo = new PDO('mysql:host=localhost;dbname=names', 'root', '',
+      [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+}
+catch (PDOException $e) {
+   echo $e->getMessage();
+   die();
+}
