d166131896e5cf295df726241885782ffcea0e5f
Peter Kidson <peterkidson-git@spamarrest.com>
2025-06-28 17:49:04 +0100

    198 start ... but debugger problem

--- 20-NAME-EXPLORER/index.php
+++ 20-NAME-EXPLORER/index.php
@@ -1,23 +1,14 @@
 <?php
 
-require __DIR__ . '/inc/all.inc.php';
+//function render($view)
+//{
+//    ob_start();
+//    require __DIR__ . '/views/pages/' . $view . '.php';
+//
+//    $contents = ob_get_clean();
+//}
+//
+//render('index.view');
 
-
-$overview = fetchNamesOverview();
-
-?>
-<?php require __DIR__ . '/views/header.php'; ?>
-
-
-<h2>Top Names</h2>
-<ol>
-   <?php foreach ($overview as $row): ?>
-      <li>
-          <a href="name.php?<?= e(http_build_query(['name' => $row['name']])); ?>">
-            <?= e($row['name']); ?>
-         </a>
-      </li>
-   <?php endforeach; ?>
-</ol>
-
-<?php require __DIR__ . '/views/footer.php'; ?>
+echo "hello world1";
+echo "";
---
+++ 20-NAME-EXPLORER/views/pages/index.view.php
@@ -0,0 +1,2 @@
+<h2>index.view file</h2>
+<p>a paragraph</p>
