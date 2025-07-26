83f6c1b890fc3c74eb49cc8b772305ea22b9bab9
peter <peterdkidson@googlemail.com>
2025-07-26 12:16:29 +0100

    225 CITY EXPLORER Overview

---
+++ 23-CITY-EXPLORER/inc/db-connect.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=cities;charset=utf8mb4', 'peter', 'peter', [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+}
+catch (PDOException $e) {
+    echo 'A problem occured with the database connection...' . $e->getMessage();
+    die();
+}
+$stmt = $pdo->prepare('SELECT count(*) as count FROM `worldcities`');
+$stmt->execute();
+$rs = $stmt->fetch(PDO::FETCH_ASSOC);
+var_dump($rs);
---
+++ 23-CITY-EXPLORER/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require __DIR__ . '/inc/all.inc.php';
+
+//$overview = fetchNamesOverview();
+//
+//render('index.view', ['overview' => $overview]);
