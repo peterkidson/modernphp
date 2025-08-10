2af544dce652ca9af8d1f88dfa134fd5652ad532
peter <peterdkidson@googlemail.com>
2025-08-10 10:57:50 +0100

    269 Error page

--- 28-CMS-1/src/Frontend/Controller/NotFoundController.php
+++ 28-CMS-1/src/Frontend/Controller/NotFoundController.php
@@ -2,10 +2,10 @@
 
 namespace App\Frontend\Controller;
 
-class NotFoundController
+class NotFoundController extends AbstractController
 {
 	public function error404() {
 		http_response_code(404);
-		echo "Page not found";
+		$this->render('notFound/error404',[]);
 	}
 }
