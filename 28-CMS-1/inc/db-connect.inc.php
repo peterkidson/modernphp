b45f673e87f8bf8af9a32c2e3d1177ec8e11f4bf
peter <peterdkidson@googlemail.com>
2025-08-11 07:35:32 +0100

    272 Set up database

--- 28-CMS-1/inc/db-connect.inc.php
+++ 28-CMS-1/inc/db-connect.inc.php
@@ -1,13 +1,11 @@
 <?php
-/*
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=cms;charset=utf8mb4', 'cms', '', [
+    $pdo = new PDO('mysql:host=localhost;dbname=cms;charset=utf8mb4', 'cms', 'cms', [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
 }
 catch (PDOException $e) {
     // var_dump($e->getMessage());
-    echo 'A problem occured with the database connection...';
+    echo 'A problem occured with the database connection...' . $e->getMessage();
     die();
 }
-*/
