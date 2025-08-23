9aacba10c612bdc078ce47c75158d5816973a735
peter <peterdkidson@googlemail.com>
2025-08-23 09:04:03 +0100

    286 Admin - css colours

--- 31-CMS-3/views/Admin/layouts/main.view.php
+++ 31-CMS-3/views/Admin/layouts/main.view.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="./styles/simple.css" />
+    <link rel="stylesheet" type="text/css" href="./styles/admin.css" />
     <link rel="stylesheet" type="text/css" href="./styles/custom.css" />
     <title>CMS Project</title>
 </head>
