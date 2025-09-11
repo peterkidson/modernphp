0e26a3c152f6e6353f34e2481046bf6988271c15
peter <peterdkidson@googlemail.com>
2025-09-11 07:34:04 +0100

    321 Magic methods...

---
+++ 34-OOP-4/Magic.php
@@ -0,0 +1,38 @@
+<?php
+
+class Magic {
+	public array $attributes;
+
+	public function __construct() {
+		$this->attributes = [
+			'id' => 5,
+			'title' => 'Hello World'
+		];
+	}
+
+	public function __get($key) {
+		if (isset($this->attributes[$key])) {
+			return $this->attributes[$key];
+		}
+		return null;
+	}
+	public function __set($key, $value) {
+		$this->attributes[$key] = $value;
+	}
+	public function __isset($key) {
+		return true;
+	}
+	public function __unset($key) {
+		var_dump("__unset() has been called with: {$key}");
+	}
+}
+header('Content-Type: text/plain');
+
+$aboutUs = new Magic();
+$aboutUs->content = 'This is the content';
+var_dump($aboutUs);
+
+var_dump(isset($aboutUs->content));
+unset($aboutUs->content);
+unset($aboutUs->attributes);
+var_dump($aboutUs);
