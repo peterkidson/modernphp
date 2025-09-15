69d0e455ce93b4ad43a40ebad49e26452ce46562
peter <peterdkidson@googlemail.com>
2025-09-15 06:40:16 +0100

    322 ArrayAcess and Countable

--- 34-OOP-4/ArrAccCnt.php
+++ 34-OOP-4/ArrAccCnt.php
@@ -2,10 +2,11 @@
 
 require __DIR__ . '/zfunctions.php';
 
-
 class AAC implements ArrayAccess, Countable
 {
 	public $propx = "john";
+
+	// attribute name =  $offset
 
 	public function offsetExists(mixed $offset): bool {					// isset
 		return isset($this->{$offset});
@@ -13,12 +14,12 @@ class AAC implements ArrayAccess, Countable
 	public function offsetGet(mixed $offset): mixed {						// get
 		return $this->{$offset};
 	}
-	public function offsetSet(mixed $offset, mixed $propname): void {		// set
-		$this->{$offset} = $propname;
+	public function offsetSet(mixed $offset, mixed $value): void {		// set
+		$this->{$offset} = $value;
 	}
-	public function offsetUnset(mixed $offset): void						// unset
+	public function offsetUnset(mixed $offset ): void						// unset
 	{
-		// TODO: Implement offsetUnset() method.
+		unset($this->{$offset});
 	}
 	public function count(): int {
 		return 123;
@@ -28,16 +29,20 @@ class AAC implements ArrayAccess, Countable
 header('Content-Type: text/plain');
 
 $p = new AAC();
-echo "propx: {$p->propx}\n";
+echo "->propx: {$p->propx}\n\n";
 
-foreach (['propx','junk'] as $propname) {echo @"get ->$propname : {$p->$propname}\n";}
+foreach (['propx','junk'] as $propname) {echo @"offsetGet ['$propname'] : {$p[$propname]}\n";}
+echo "\n";
 
 $p->junk = "not junk now!";   // deprecated
-foreach (['propx','junk'] as $propname) {echo @"get ->$propname : {$p->$propname}\n\n";}
-
-foreach (['propx','junk'] as $propname) {echo @"isset($propname) : " . b(isset($p->$propname)) . "\n";}
+foreach (['propx','junk'] as $propname) {echo @"offsetGet ['$propname'] : {$p[$propname]}\n";}
 echo "\n";
-unset($p->propx);
-foreach (['propx','junk'] as $propname) {echo @"isset($propname) : " . b(isset($p->$propname)) . "\n";}
+
+foreach (['propx','junk'] as $propname) {echo @"isset(['$propname']) : " . b(isset($p[$propname])) . "\n";}
+echo "\n";
+
+foreach (['propx','junk'] as $propname) {unset($p[$propname]);}
+foreach (['propx','junk'] as $propname) {echo @"isset(['$propname']) : " . b(isset($p[$propname])) . "\n";}
+echo "\n";
 
 
